dbec2c330cd74419855de9c78e75d1cd8ebb7214
Helena Chi <hchi124@gmail.com>
2020-07-14 13:42:19 -0700

    can successfully log into HomeScreen

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react'
-import { Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
 import { firebase } from './src/firebase/config'
 import {decode, encode} from 'base-64'
+import { set } from 'react-native-reanimated';
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
 
@@ -16,45 +16,46 @@ export default function App() {
   const [loading, setLoading] = useState(true)
   const [user, setUser] = useState(null)
 
-  // if (loading) {	
-  //   return (	
-  //     // <Text>loading...</Text>
-  //     <></>	
-  //   )	
-  // }
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data()
+            setLoading(false)
+            setUser(userData)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        console.log("no user")
+        setLoading(false)
+      }
+    })
+    setLoading(false)
+  }, []);
 
-  // useEffect(() => {
-  //   const usersRef = firebase.firestore().collection('users');
-  //   console.log("in useEffect")
-  //   firebase.auth().onAuthStateChanged(user => {
-  //     if (user) {
-  //       usersRef
-  //         .doc(user.uid)
-  //         .get()
-  //         .then((document) => {
-  //           const userData = document.data()
-  //           setLoading(false)
-  //           setUser(userData)
-  //         })
-  //         .catch((error) => {
-  //           setLoading(false)
-  //         });
-  //     } else {
-  //       setLoading(false)
-  //     }
-  //   });
-  // }, []);
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        { user ? console.log(user.id) : console.log("you wished this worked bitch")}
-        <>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Registration" component={RegistrationScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </>
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+  if (loading) {
+    return (
+      <></>
+    )
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <>
+            <Stack.Screen name="Login" component={LoginScreen} />
+            <Stack.Screen name="Registration" component={RegistrationScreen} />
+            <Stack.Screen name="Home">
+              {props => <HomeScreen {...props} />}
+            </Stack.Screen>
+          </>
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
