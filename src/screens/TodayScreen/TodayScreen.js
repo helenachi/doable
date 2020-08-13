0f4468bbd0d6bcbf70d1f215f475f99add2c2db3
Helena Chi <hchi124@gmail.com>
2020-08-13 13:38:02 -0500

    user goal and task successfuly retrieved and displayed on todayscreen

--- src/screens/TodayScreen/TodayScreen.js
+++ src/screens/TodayScreen/TodayScreen.js
@@ -7,10 +7,9 @@ import { firebase } from "../../firebase/config";
 import { AppLoading } from "expo";
 
 export default function TodayScreen(props) {
-  console.log(props.user);
   const [loading, setLoading] = useState(true);
   const [userGoals, setUserGoals] = useState(null);
-  // const [goalTasks, setGoalTasks] = useState(null);
+  const [goalTasks, setGoalTasks] = useState(null);
   let randomGoal = "goal" + props.user.randomGoal.toString();
 
   useEffect(() => {
@@ -24,8 +23,16 @@ export default function TodayScreen(props) {
         .then(function (doc) {
           if (doc.exists) {
             setUserGoals(doc.data()[props.user.id]);
-            // console.log("user goals set!");
-            // console.log("Document data:", doc.data()[props.user.id]);
+            const goalTasksRef = firebase
+              .firestore()
+              .collection("goals")
+              .doc(userGoals[randomGoal]);
+            goalTasksRef.get().then(function (doc) {
+              if (doc.exists) {
+                console.log("goalTasks data exists!");
+                setGoalTasks(doc.data());
+              }
+            });
           } else {
             // doc.data() will be undefined in this case
             console.log("No such document!");
@@ -34,34 +41,24 @@ export default function TodayScreen(props) {
         .catch(function (error) {
           console.log("Error getting document:", error);
         });
-      if (userGoals) {
-        console.log("userGoals:", userGoals);
+      if (userGoals && goalTasks) {
+        // console.log("userGoals:", userGoals);
         setLoading(false);
       }
     }
-
-    // const goalTasksRef = firebase
-    //   .firestore()
-    //   .collection("goals")
-    //   .doc(userGoals[randomGoal]);
-    // goalTasksRef.get().then(function (doc) {
-    //   if (doc.exists) {
-    //     console.log("Goals data:", doc.data());
-    //   }
-    // });
-  }, [userGoals]);
+  }, [userGoals, goalTasks]);
 
   if (loading) {
     return <AppLoading />;
   } else {
     console.log("userGoals data:", userGoals);
+    console.log("goalTasks: ", goalTasks);
     return (
       <View style={styles.container}>
         <Text style={styles.titleText}>Today's{"\n"}Doable</Text>
         <Text style={styles.goalText}>{userGoals[randomGoal]}</Text>
         <Text style={styles.taskText}>
-          Set up a time to catch up with your family and ask about their
-          highlights and lowlights for the past week!
+          {goalTasks.tasks[goalTasks.randomTask]}
         </Text>
         <Button title="Start Time"></Button>
       </View>
