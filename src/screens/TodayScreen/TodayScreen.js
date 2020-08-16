cf7499dfa5cc328e238f2e5bcf4d2894e306ef26
Helena Chi <hchi124@gmail.com>
2020-08-16 02:12:39 -0500

    play button on:

--- src/screens/TodayScreen/TodayScreen.js
+++ src/screens/TodayScreen/TodayScreen.js
@@ -5,12 +5,20 @@ import { Ionicons } from "@expo/vector-icons";
 import styles from "./styles";
 import { firebase } from "../../firebase/config";
 import { AppLoading } from "expo";
-import CircleTimer from "react-native-circle-timer";
+import { AnimatedCircularProgress } from "react-native-circular-progress";
 
 export default function TodayScreen(props) {
   const [loading, setLoading] = useState(true);
   const [userGoals, setUserGoals] = useState(null);
   const [goalTasks, setGoalTasks] = useState(null);
+  /**
+   * 0: not started
+   * 1: in progress
+   * 2: paused
+   * 3: complete
+   */
+  const [completionStatus, setCompletionStatus] = useState(0);
+  let randomGoal = "goal" + props.user.randomGoal.toString();
 
   useEffect(() => {
     if (loading) {
@@ -18,30 +26,27 @@ export default function TodayScreen(props) {
         .firestore()
         .collection("monthly_goals")
         .doc(props.current);
-      monthlyGoalsRef
-        .get()
-        .then(function (doc) {
-          if (doc.exists) {
-            setUserGoals(doc.data()[props.user.id]);
-            const goalTasksRef = firebase
-              .firestore()
-              .collection("goals")
-              .doc(userGoals[randomGoal]);
-            goalTasksRef.get().then(function (doc) {
-              if (doc.exists) {
-                console.log("goalTasks data exists!");
-                setGoalTasks(doc.data());
-              } else {
-                console.log("No such document!");
-              }
-            });
-          } else {
-            console.log("No such document!");
-          }
-        })
-        .catch(function (error) {
-          console.log("Error getting document:", error);
-        });
+      monthlyGoalsRef.get().then(function (doc) {
+        if (doc.exists) {
+          setUserGoals(doc.data()[props.user.id]);
+          const goalTasksRef = firebase
+            .firestore()
+            .collection("goals")
+            .doc(userGoals[randomGoal]);
+          goalTasksRef.get().then(function (doc) {
+            if (doc.exists) {
+              setGoalTasks(doc.data());
+            } else {
+              console.log("No such document!");
+            }
+          });
+        } else {
+          console.log("No such document!");
+        }
+      });
+      // .catch(function (error) {
+      //   console.log("Error getting document:", error);
+      // });
       if (userGoals && goalTasks) {
         setLoading(false);
       }
@@ -49,7 +54,6 @@ export default function TodayScreen(props) {
   }, [userGoals, goalTasks]);
 
   const markTaskComplete = () => {
-    let randomGoal = "goal" + props.user.randomGoal.toString();
     const doableToUpdate =
       "completedTasks" +
       "." +
@@ -66,6 +70,16 @@ export default function TodayScreen(props) {
     });
   };
 
+  const playButton = (
+    <TouchableOpacity>
+      <Ionicons name="ios-play-circle" size={20} color="gray" />
+    </TouchableOpacity>
+  );
+
+  // const completionComponent = () => {
+  //   return <AppLoading />;
+  // };
+
   if (loading) {
     return <AppLoading />;
   } else {
@@ -78,7 +92,20 @@ export default function TodayScreen(props) {
         <Text style={styles.taskText}>
           {goalTasks.tasks[goalTasks.randomTask]}
         </Text>
-        <Button onPress={markTaskComplete} title="Mark Complete"></Button>
+        {playButton}
+        {/* <Button onPress={markTaskComplete} title="Mark Complete"></Button>
+        <View style={{ alignSelf: "center" }}>
+          <AnimatedCircularProgress
+            size={120}
+            width={15}
+            fill={100}
+            rotation={0}
+            tintColor="#00e0ff"
+            onAnimationComplete={() => console.log("onAnimationComplete")}
+            backgroundColor="#3d5875"
+            duration={10000} // how fast it fills up in ms
+          />
+        </View> */}
       </View>
     );
   }
