c5e9ccc91fa913f63efe2423a8a5d8cf3b5c0840
Helena Chi <hchi124@gmail.com>
2020-08-28 21:49:38 -0500

    add axiom

--- src/screens/CalendarScreen/CalendarScreen.js
+++ src/screens/CalendarScreen/CalendarScreen.js
@@ -4,7 +4,7 @@ import { unstable_renderSubtreeIntoContainer } from "react-dom";
 import { Calendar, CalendarList, Agenda } from "react-native-calendars";
 import { Ionicons } from "@expo/vector-icons";
 import styles from "./styles";
-import { firebase } from "../../firebase/config";
+import { firebase, axios } from "../../firebase/config";
 import { AppLoading } from "expo";
 import {
   useFonts,
@@ -22,56 +22,54 @@ export default function CalendarScreen(props) {
   const [goalColors, setGoalColors] = useState(null);
   const [userDates, setUserDates] = useState(null);
   const userGoalKeys = ["goal1", "goal2", "goal3"];
+  const [appData, setAppData] = useState(null);
+
+  // console.log("userGoals: ", userGoals);
+  // console.log("goalColors: ", goalColors);
+  // console.log("userDates: ", userDates);
 
   useEffect(() => {
     if (loading) {
-      const monthlyGoalsRef = firebase
-        .firestore()
-        .collection("monthly_goals")
-        .doc(props.current);
-      monthlyGoalsRef
-        .get()
-        .then(function (doc) {
-          if (doc.exists) {
-            setUserGoals(doc.data()[props.user.id]);
-            const goalColorsRef = firebase
-              .firestore()
-              .collection("appData")
-              .doc("goalColors");
-            goalColorsRef.get().then(function (doc) {
-              if (doc.exists) {
-                setGoalColors(doc.data());
-                const markedDatesRef = firebase
-                  .firestore()
-                  .collection("markedDates")
-                  .doc(props.user.id);
-                markedDatesRef.get().then(function (doc) {
-                  if (doc.exists) {
-                    setUserDates(doc.data()["dates"]);
-                  } else {
-                    console.log("No such document!");
-                  }
-                });
-              } else {
-                console.log("No such document!");
-              }
-            });
-          } else {
-            console.log("No such document!");
-          }
-        })
-        .catch(function (error) {
-          console.log("Error getting document:", error);
-        });
-      if (userGoals && goalColors && userDates) {
-        setLoading(false);
-      }
+      const fetchAppData = async () => {
+        const data = await axios.get("appData");
+        setAppData(data);
+        console.log(appData);
+      };
+      // const fetchUserGoals = async () => {
+      //   const data = await axios.get(`monthly_goals/${props.user.id}`);
+      //   setUserGoals(data);
+      //   console.log("userGoals: ", data);
+      // };
+
+      // const fetchGoalColors = async () => {
+      //   const data = await axios.get("appData/goalColors");
+      //   setGoalColors(data);
+      //   console.log("goalColors: ", data);
+      // };
+
+      // const fetchUserDates = async () => {
+      //   const data = await axios.get(`markedDates/${props.user.id}/dates`);
+      //   setUserDates(data);
+      //   console.log("userDates: ", data);
+      // };
+
+      // fetchUserGoals();
+      // fetchGoalColors();
+      // fetchUserDates();
+      fetchAppData();
+      console.log("should have run all fetch()'s");
+    }
+    if (userGoals && goalColors && userDates) {
+      setLoading(false);
     }
   }, [userGoals, goalColors, userDates]);
 
   if (loading) {
     return <AppLoading />;
   } else {
+    // console.log("userGoals: ", userGoals);
+    // console.log("goalColors: ", goalColors);
+    // console.log("userDates: ", userDates);
     return (
       <>
         <Text style={styles.title}>Happy{"\n"}Calendar</Text>
